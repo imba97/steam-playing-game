d79bd18cef3780d482e94d692c75aa01d3191e37
imba97 <mail@imba97.cn>
2025-03-13 00:02:57 +0800

    feat: export response types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+export type {
+  GmaeDetailResponse,
+  PlayerSummaryResponse,
+  UserPlayingGameResponse
+} from './data'
+
 export { useRequest as useSteamApi } from './request'
 export { getUserPlayingGame } from './user-playing-game'
